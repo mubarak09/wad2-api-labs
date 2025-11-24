726175f4325546427357b10480355128e5a6366a
Your Name <you@example.com>
2025-11-24 21:35:33 +0000

    Express Lab - exercise done

--- tasky-api/api/tasks/tasksData.js
+++ tasky-api/api/tasks/tasksData.js
@@ -1,8 +1,39 @@
+const currentTime = new Date().toISOString();
+
 export const tasksData = {
     page: 1,
     total_pages: 1,
     total_results: 3,
-    tasks: [{ id: "1", title: "Dishes", description: "Empty dishwasher", deadline: "Today", done: false, priority: "low" },
-    { id: "2", title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false, priority: "medium" },
-    { id: "3", title: "Tidy up", deadline: "Today", done: false, priority: "high" }]
+    tasks: [
+        { 
+            id: "1", 
+            title: "Dishes", 
+            description: "Empty dishwasher", 
+            deadline: "Today", 
+            done: false, 
+            priority: "low",
+            created_at: currentTime,
+            updated_at: currentTime
+        },
+        { 
+            id: "2", 
+            title: "Laundry", 
+            description: "Fold clothes and put away", 
+            deadline: "Tomorrow", 
+            done: false, 
+            priority: "medium",
+            created_at: currentTime,
+            updated_at: currentTime
+        },
+        { 
+            id: "3", 
+            title: "Tidy up", 
+            description: "", 
+            deadline: "Today", 
+            done: false, 
+            priority: "high",
+            created_at: currentTime,
+            updated_at: currentTime
+        }
+    ]
 };
