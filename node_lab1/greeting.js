a980a240c31be74f2b2f208486677136e7ad7433
Your Name <you@example.com>
2025-11-24 13:48:27 +0000

    Completed node lab 1!

--- node_lab1/greeting.js
+++ node_lab1/greeting.js
@@ -1,6 +1,10 @@
 const greeting = {
-    en: "Hello World!",
-    fr: "Bonjour!"
+    en: 'Hello World!',
+    fr: 'Bonjour!',
+    es: 'Hola!',
+    de: 'Hallo!',
+    ga: 'Dia duit',
+    it: 'Ciao!',
 };
 
 export default greeting;
